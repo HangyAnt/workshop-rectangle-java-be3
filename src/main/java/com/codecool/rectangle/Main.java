fc0cce442282d8cdccba0f8ca64a40e7f4b1274f
HangyAnt <gabor.hangyasi@gmail.com>
2021-01-05 09:27:04 +0100

    two spaces added to rectangle2 constructor arguments :)

--- src/main/java/com/codecool/rectangle/Main.java
+++ src/main/java/com/codecool/rectangle/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println("Rectangle1 height: " + rectangle1.getHeight());
         System.out.println("Rectangle1 width: " + rectangle1.getWidth());
         System.out.println("Rectangle1 area: " + rectangle1.calculateArea());
-        Rectangle rectangle2 = new Rectangle(4,3,12);
+        Rectangle rectangle2 = new Rectangle(4, 3, 12);
         System.out.println("Rectangle2 height: " + rectangle2.getHeight());
         System.out.println("Rectangle2 width: " + rectangle2.getWidth());
         System.out.println("Rectangle2 area: " + rectangle2.calculateArea());
