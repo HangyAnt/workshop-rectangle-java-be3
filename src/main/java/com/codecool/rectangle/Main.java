87b68e1a0ddedcd608c815271769adf6a6605fa6
HangyAnt <gabor.hangyasi@gmail.com>
2021-01-05 10:02:47 +0100

    roundedRectangle instance created in Main

--- src/main/java/com/codecool/rectangle/Main.java
+++ src/main/java/com/codecool/rectangle/Main.java
@@ -6,11 +6,16 @@ public class Main {
         Rectangle rectangle1 = new Rectangle(5, 8);
         System.out.println("Rectangle1 height: " + rectangle1.getHeight());
         System.out.println("Rectangle1 width: " + rectangle1.getWidth());
-        System.out.println("Rectangle1 area: " + rectangle1.calculateArea());
+        System.out.println("Rectangle1 area: " + rectangle1.calculateArea() + "\n");
         Rectangle rectangle2 = new Rectangle(4, 3, 12);
         System.out.println("Rectangle2 height: " + rectangle2.getHeight());
         System.out.println("Rectangle2 width: " + rectangle2.getWidth());
-        System.out.println("Rectangle2 area: " + rectangle2.calculateArea());
+        System.out.println("Rectangle2 area: " + rectangle2.calculateArea() + "\n");
+        RoundedRectangle roundedRectangle = new RoundedRectangle(4, 6, 1);
+        System.out.println("RoundedRectangle height: " + roundedRectangle.getHeight());
+        System.out.println("RoundedRectangle width: " + roundedRectangle.getWidth());
+        System.out.println("RoundedRectangle radius: " + roundedRectangle.getRoundRadius());
+        System.out.println("RoundedRectangle area: " + roundedRectangle.calculateArea() + "\n");
     }
 
 }
