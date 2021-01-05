70d569ce5ab47283e564fcca61087be95c7fef9e
HangyAnt <gabor.hangyasi@gmail.com>
2021-01-05 09:14:28 +0100

    size field, getHeight, getWidth created

--- src/main/java/com/codecool/rectangle/Rectangle.java
+++ src/main/java/com/codecool/rectangle/Rectangle.java
@@ -4,10 +4,24 @@ public class Rectangle {
 
     private int height;
     private int width;
+    private int size;
 
     Rectangle(int height, int width) {
         this.height = height;
         this.width = width;
+    }
+
+    Rectangle(int height, int width, int size) {
+        this.height = height;
+        this.width = width;
+    }
+
+    int getHeight() {
+        return this.height;
+    }
+
+    int getWidth() {
+        return this.width;
     }
 
 }
